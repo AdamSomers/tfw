4d080fe0f6a752333dc217d808fe9e896cfa3386
Adam Somers <adam@jauntvr.com>
2017-12-26 21:03:51 -0800

    update to opengl 4.1 API, perspective scene

--- app/main.cpp
+++ app/main.cpp
@@ -1,42 +1,53 @@
-#include <iostream>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "linmath.h"
 
 #include "Scene.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <iostream>
 using namespace std;
 
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
-};
+  GLfloat vertices[] = {
+    -1.f, -1.f, 0.f,
+     1.f,  -1.f, 0.f,
+     0.f, 1.f, 0.f
+  };
+
+  GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f, 1.0f,
+    0.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f, 1.0f
+  };
+
 static const char* vertex_shader_text =
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
+{
+  "#version 400\n"\
+
+  "layout(location=0) in vec3 in_Position;\n"\
+  "layout(location=1) in vec4 in_Color;\n"\
+  "uniform mat4 modelMat;\n"\
+  "uniform mat4 viewMat;\n"\
+  "uniform mat4 projMat;\n"\
+  "out vec4 ex_Color;\n"\
+
+  "void main(void)\n"\
+  "{\n"\
+  "  gl_Position = projMat * viewMat * modelMat * vec4(in_Position.x, in_Position.y, in_Position.z, 1.0);\n"\
+  "  ex_Color = in_Color;\n"\
+  "}\n"
+};
 static const char* fragment_shader_text =
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
+{
+  "#version 400\n"\
+
+  "in vec4 ex_Color;\n"\
+  "out vec4 out_Color;\n"\
+
+  "void main(void)\n"\
+  "{\n"\
+  "  out_Color = ex_Color;\n"\
+  "}\n"
+};
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -48,15 +59,20 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 int main(int argc, const char** argv)
 {
-   cout << "tfw application start" << endl;
+    cout << "tfw application start" << endl;
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+
+    GLuint vaoId;
+    GLuint colorBuffer;
+
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
     {
@@ -68,9 +84,8 @@ int main(int argc, const char** argv)
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     glfwSwapInterval(1);
     // NOTE: OpenGL error checks have been omitted for brevity
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // create the shader
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
@@ -81,15 +96,28 @@ int main(int argc, const char** argv)
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(float) * 5, (void*) 0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(float) * 5, (void*) (sizeof(float) * 2));
+
+    // create the vertex array
+    glGenVertexArrays(1, &vaoId);
+    glBindVertexArray(vaoId);
+
+    // create the vbo
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // set vertex metadata for attributes (num elements, type, stride, offset)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    GLuint modelMatLocation = glGetUniformLocation(program, "modelMat");
+    GLuint viewMatLocation = glGetUniformLocation(program, "viewMat");
+    GLuint projMatLocation = glGetUniformLocation(program, "projMat");
 
     tfw::Scene scene;
 
@@ -97,17 +125,27 @@ int main(int argc, const char** argv)
     {
         float ratio;
         int width, height;
-        mat4x4 m, p, mvp;
+        mat4x4 m, v, p;
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
         mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
+        mat4x4_identity(v);
+        mat4x4_perspective(p,
+                           1,
+                           width / (float)height,
+                           1.f, 60.0);
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+            
+        // move the eye back 2m
+        mat4x4_translate(v, 0.f, 0.f, -2.f);
+        // spin the model
+        mat4x4_translate(m, -1.f, 0.f, -1.f);
+        mat4x4_rotate_Y(m, m, (float) glfwGetTime());
+        glUniformMatrix4fv(modelMatLocation, 1, GL_FALSE, (const GLfloat*) m);
+        glUniformMatrix4fv(viewMatLocation, 1, GL_FALSE, (const GLfloat*) v);
+        glUniformMatrix4fv(projMatLocation, 1, GL_FALSE, (const GLfloat*) p);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         scene.render();
